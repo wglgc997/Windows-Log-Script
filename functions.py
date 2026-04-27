0e5040faea0f4952ab28ff656635060611d07b44
Wagner Carvalho <me@example.com>
2026-04-27 19:44:44 -0300

    main, function file updated

--- functions.py
+++ functions.py
@@ -1,15 +1,31 @@
 import win32evtlog
+import subprocess
+from pathlib import Path
 
-def read_log(server,log_type):
-    flags = (win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ)
-    handle = win32evtlog.OpenEventLog(server,log_type)
+
+def read_log(server, log_type):
+    readLog = (win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ)
+    eventLogList = win32evtlog.OpenEventLog(server, log_type)
 
     try:
-        events = win32evtlog.ReadEventLog(handle, flags, 0)
+        events = win32evtlog.ReadEventLog(eventLogList, readLog, 0 )
         for event in events[:10]:
             print(f"Time {event.TimeGenerated}")
             print(f"ID: {event.EventID}")
             print(f"Category: {event.EventCategory}")
             print("--" * 10)
     finally:
-        win32evtlog.CloseEventLog(handle)
+        win32evtlog.CloseEventLog(eventLogList)
+
+def create_file():
+    file_path = Path(r'C:\Users\ResTIC55\PycharmProjects\WinLogProject')
+    log_file =  file_path / "system_log.txt"
+    backup_file =  file_path / "backup.txt"
+
+    file_path.mkdir(parents=True, exist_ok=True)
+    log_file.write_text("Teste")
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import os
 import win32evtlog
 import win32evtlogutil
-from functions import read_log
+from functions import create_file, read_log
 
 server = "localhost"
 log_type = "System, Security, Application"
 
 read_log(server, log_type)
-
-
-
-
+create_file()
